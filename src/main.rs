4de47ad0ecb15f90406b6f9a9c508166594812b6
Mahesh Aravind V <gameprovenky@gmail.com>
2022-08-28 15:09:41 +0530

    Find average for 5x5 box

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,17 @@
 extern crate raster;
 fn main(){
     let image = raster::open("test.png").unwrap();
-    let mut resim = raster::Image::blank(image.width, image.height);
-    for w in 0..image.width{
-        for h in 0..image.height{
-            let pixel = image.get_pixel(w,h).unwrap();
-            let avg = pixel.r/3 + pixel.b/3 + pixel.g/3;
-            let color = raster::Color {r:avg,g:avg,b:avg,a:255};
+    let mut resim = raster::Image::blank(image.width/5, image.height/5);
+    for w in 0..image.width/5{
+        for h in 0..image.height/5{
+            let mut avg = 0;
+            for xbox in 0..5{
+                for ybox in 0..5{
+                    let color = image.get_pixel(w*5+xbox, h*5+ybox).unwrap();
+                    avg += (color.r/3 + color.b/3 + color.g/3)/25
+                }
+            }
+            let color = raster::Color {r : avg,b : avg, g : avg,a : 255};
             resim.set_pixel(w, h, color).unwrap();
         }
     }
